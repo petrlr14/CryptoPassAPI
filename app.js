8c25c950f3160c3cd2f79f4006ff0158fdf9a729
CarlosDubon <00058016@uca.edu.sv>
2018-11-10 11:22:31 -0600

    axios y rutas

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const routesUser = require('./routes/userRoute');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+
 app.use(bodyParser.urlencoded({useNewUrlParser: false}));
 app.use(bodyParser.json());
 
