43116e30d51f2a1c5c2579e6dd1ce3ec4a530e10
CarlosDubon <00058016@uca.edu.sv>
2018-11-10 17:04:09 -0600

    fix

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const config = require("./config");
 
+app.listen(config.port,()=>{
+    console.log(`Servidor corriendo puerto: ${config.port}`);
+});
 mongoose.connect(config.bd,(err,res)=>{
     if(err) throw err
     console.log("conexion establecida");
 
     
 });
-app.listen(config.port,()=>{
-    console.log(`Servidor corriendo puerto: ${config.port}`);
-});
+
