d4f7c1b2ffa9f464d6e4c0078f5786304ee550b0
CarlosDubon <00058016@uca.edu.sv>
2018-11-10 22:33:33 -0600

    conect mongo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports ={
-    port: process.env.PORT || 8000,
-    bd: process.env.MONGODB || "mongodb+srv://Crypty:Andresp3ndejo@cryptopasscloud-xpegu.gcp.mongodb.net/test?retryWrites=true"
+    port: process.env.PORT || 3001,
+    bd: process.env.MONGODB || "mongodb://Crypty:Andresp3ndejo@cryptopasscloud-shard-00-00-xpegu.gcp.mongodb.net:27017,cryptopasscloud-shard-00-01-xpegu.gcp.mongodb.net:27017,cryptopasscloud-shard-00-02-xpegu.gcp.mongodb.net:27017/test?ssl=true&replicaSet=CryptoPassCloud-shard-0&authSource=admin&retryWrites=true"
 }
