1f974d51e88fd8e08a445dbd321ed03dab0f5c14
CarlosDubon <00058016@uca.edu.sv>
2018-11-10 23:09:42 -0600

    conect mongo

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const routesUser = require('./routes/userRoute');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://cryptopass-api.herokuapp.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
