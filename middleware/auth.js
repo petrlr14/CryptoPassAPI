5d452cf3f7e8efcb9e623835e4a7467bcecef9db
CarlosDubon <00058016@uca.edu.sv>
2018-11-11 14:05:56 -0600

    fix authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ function isAuth(req,res,next){
     if(!req.headers.authorization){
         return res.status(403).send({message:'No authorization'})
     }
-    const token = req.headers.autorization.split(" ")[1];
+    const token = req.headers.authorization.split(" ")[1];
     
     service.decodeToken(token)
         .then(response =>{
