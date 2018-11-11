9b9f97273c01b679ebebded410daa7961e572edd
CarlosDubon <00058016@uca.edu.sv>
2018-11-10 19:06:16 -0600

    db

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports ={
     port: process.env.PORT || 8000,
-    bd: process.env.MONGODB || "mongodb://localhost:27017/cryptopass"
+    bd: process.env.MONGODB || "mongodb+srv://Crypty:Andresp3ndejo@cryptopasscloud-xpegu.gcp.mongodb.net/test?retryWrites=true"
 }
