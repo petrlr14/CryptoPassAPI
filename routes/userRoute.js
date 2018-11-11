25723c4cd1577118f30f9519700052da3c1c4e0b
CarlosDubon <00058016@uca.edu.sv>
2018-11-10 23:21:46 -0600

    conect mongo

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const UserCtrl = require('../controllers/userController');
 const api = express.Router();
+const app = express();
+
+console.log('making this');
+app.use(cors());
 
 api.post('/insertUser',UserCtrl.signUp);
 api.get('/users',UserCtrl.getAllUsers);
