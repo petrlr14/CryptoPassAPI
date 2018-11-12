abb5ee26a46c9ef6c1c5f4a8d4ea1dc1b56e4e69
CarlosDubon <00058016@uca.edu.sv>
2018-11-12 10:31:49 -0600

    fix

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 'use strict'
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors =require('cors');
 const app = express();
 const routesUser = require('./routes/userRoute');
 const routesAuth = require('./routes/authRoute');
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://cryptopass-api.herokuapp.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
-
+app.use(cors());
 
 app.use(bodyParser.urlencoded({useNewUrlParser: false}));
 app.use(bodyParser.json());
