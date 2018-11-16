40c6434470a8a858ef5bb094fc0aad8400a15820
CarlosDubon <00058016@uca.edu.sv>
2018-11-16 13:42:35 -0600

    new Account (Encrypted)

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,10 +1,12 @@
 'use strict'
 const Account = require("../modules/account");
+const AES = require('crypto-js/aes');
 const User = require('../modules/user');
 
 function newAccount(req,res){
-    let userId = req.body._id;
-    let newAccount = new Account();
+    var userId = req.body._id;
+    var privateKey="";
+    var newAccount = new Account();
     newAccount.wallpaper = req.body.wallpaper ;
     newAccount.name = req.body.name ;
     newAccount.description = req.body.description ;
@@ -12,24 +14,33 @@ function newAccount(req,res){
     newAccount.login = req.body.login;
     newAccount.password= req.body.password;
 
-    newAccount.save((err,account)=>{
-        if(err) return res.status(500).send({message:`somthing is wrong ${err}`});
+    User.findById(userId,(err,usr)=>{
+        if(err) return res.status(500).send({message:'internal error'})
+        if(!usr) return res.status(404).send({message:"User not found, please sign"})
+        privateKey = usr.password.substring(0,31);
+        newAccount.password = AES.encrypt(newAccount.password,privateKey);
+        newAccount.login = AES.encrypt(newAccount.login,privateKey);
         
-        User.findOne({'_id':userId},(err,user)=>{
-            if(err) return res.status(500).send({message:`Something is wrong ${err}`});
-            if(!user) return res.status(404).send({message:`User not exist`});
-            user.user_accounts.forEach(element => {
-                if(element.id_account == account._id){
-                    user.user_accounts.splice(user.user_accounts.indexOf(element),1);
-                }
-            });
-            user.user_accounts.push({id_account:account._id})
-            user.save((err,usrUpdate)=>{
+        newAccount.save((err,account)=>{
+            if(err) return res.status(500).send({message:`somthing is wrong ${err}`});
+            
+            User.findOne({'_id':userId},(err,user)=>{
                 if(err) return res.status(500).send({message:`Something is wrong ${err}`});
-                res.status(200).send({message:'account created successfuly'})
+                if(!user) return res.status(404).send({message:`User not exist`});
+                user.user_accounts.forEach(element => {
+                    if(element.id_account == account._id){
+                        user.user_accounts.splice(user.user_accounts.indexOf(element),1);
+                    }
+                });
+                user.user_accounts.push({id_account:account._id})
+                user.save((err,usrUpdate)=>{
+                    if(err) return res.status(500).send({message:`Something is wrong ${err}`});
+                    res.status(200).send({message:'account created successfuly'})
+                })
             })
         })
-    })
+    })    
+    
 
 }
 
--- modules/account.js
+++ modules/account.js
@@ -1,14 +1,32 @@
 'use strict'
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const AES = require('crypto-js/aes');
 
 const AccountSchema = new Schema({
-    wallpaper: String,
-    name: String,
-    description: String,
-    url:String,
-    login: String,
-    password: String
+    wallpaper: {
+        type:String
+    },
+    name: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    description: {
+        type:String
+    },
+    url:{
+        type:String
+    },
+    login: {
+        type:String,
+        required:true
+    },
+    password: {
+        type:String,
+        required:true
+    }
+     
 });
 
 module.exports = mongoose.model('Account',AccountSchema);
